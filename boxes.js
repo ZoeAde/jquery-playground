48d980d51cbbfe929ee761044e03c7266d8cf9a6
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-01 16:36:53 -0600

    change css for boxes

--- boxes.js
+++ boxes.js
@@ -24,4 +24,19 @@ $('#row2').children(2);
 
 //Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
 
+
+//Add an on click handler to the container div. Console log the x and y position of the click.
+
+
+//Add links inside all red box divs that take the user to galvanize. Then add an on click handler that alerts the user that you can never leave the page. Make sure the user won't leave the page after the alert!
+
+
+//For all box divs, add a click handler that adds an image of a cute puppy to the box. If the image is clicked again, remove the cute puppy.
+
+
+//Write a click handler on the conatainer div. The click handler should turn the container background to black and the background of the original div that was clicked to white. If the user original div that was clicked happened to be the container div, change the background of the container div to lime green. You should not use any selectors for this exercise. You can do it completely with what is given to you in the event callback.
+
+
+
+
 });
